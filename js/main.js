ee39b073f78ba63be84e9c83f3d79d243dd78337
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2023-01-30 22:57:14 +0700

    update: add section footet, header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 /**
  * desc: berisi logic website seperti (slider, get api, dsb)
  */
+const navA = document.querySelectorAll('nav ul li a');
+
+const setActiveMenu = () => {
+  setTimeout(() => {
+    for (i = 0; i < navA.length; i++) {
+      navA[i].classList.remove('nav-active');
+      if (window.location.href.indexOf(navA[i].href) >= 0) {
+        navA[i].className = 'nav-active';
+      }
+    }
+  }, 100);
+};
+
+window.onload = setActiveMenu();
